7a510ec6165ea71fce1cea135499c1c615635326
tmkryb <tmkryb@gmail.com>
2018-11-21 19:22:09 +0100

    use effect

---
+++ src/containers/UseEffect/index.js
@@ -0,0 +1,21 @@
+import React, {useEffect, useState} from 'react'
+
+export default function UseEffect(){
+    
+    const [name, setName] = useState("Burgeriusz")
+    const [surname, setSurname] = useState("Hermandy")
+    useEffect(() => {        
+        document.title = `${name} ${surname}`;   
+    }, [surname])
+    
+    return(
+        <>
+            <p>
+                Users name: <input defaultValue={name} onChange={(e) => setName(e.target.value)}></input>                
+            </p>
+            <p>
+                Users surname: <input defaultValue={surname} onChange={(e) => setSurname(e.target.value)}></input>
+            </p>
+        </>
+    )
+}
--- src/routes.jsx
+++ src/routes.jsx
@@ -3,12 +3,14 @@ import {Switch, Route} from 'react-router'
 import Global from './containers/GlobalState';
 import Local from './containers/LocalState';
 import GlobalNonReducer from './containers/GlobalNonReducer';
+import UseEffect from './containers/UseEffect/index';
 export default function(){
     return (
         <Switch>
             <Route path="/Global" component={Global}></Route>
             <Route path="/Local" component={Local}></Route>
             <Route path="/Hairs" component={GlobalNonReducer}></Route>
+            <Route path="/Effect" component={UseEffect}></Route>
         </Switch>
     )
 }
